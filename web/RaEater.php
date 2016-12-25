03135791f5b576965ad38176acfb930b877aa711
EGOmaniack <EGOmaniack@mail.ru>
2016-12-25 15:34:53 +1000

    Заполняем Ra

---
+++ web/RaEater.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EGOmaniack
+ * Date: 25.12.2016
+ * Time: 13:36
+ */
+$mysqli = new mysqli('localhost', 'root','','FirstBD');
+if ($mysqli->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+$mysqli->set_charset("utf8");
+if(file_exists('111.txt')) {
+
+    $Ra;
+
+    $handle = fopen("111.txt", "r");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            if (strpos($line, "BEGIN") === false && strpos($line, "END") === false) {
+                if (strpos($line, "Ra") > 0) {
+                    $raline = substr($line, strpos($line, "Ra"));
+                } else {
+                    $raline = "";
+                }
+
+                if (strlen($raline) > 0) {
+                    $exp = explode("&", $raline);
+                    $exp[1] = (int)$exp[1];
+                    $Ra = implode("", $exp);
+                } else {
+                    $line = str_replace(',', '.', $line);
+                    $s = preg_replace("/[^0-9\.]/", "", $line);
+                    $item = (float)$s;
+                    $sqls = "INSERT INTO ras(value, Ragroup) VALUES (" . $item . ",'" . $Ra . "');";
+                    $mysqli->query($sqls);
+                }
+
+            }
+        }
+        $mysqli->commit();
+echo "Готово)";
+        fclose($handle);
+        unlink('111.txt');
+    }
+}
