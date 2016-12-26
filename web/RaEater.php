afc64f4b85b8108a9262b0e6b47c0b90c444e709
EGOmaniack <EGOmaniack@mail.ru>
2016-12-26 12:39:29 +1000

    change bd

--- web/RaEater.php
+++ web/RaEater.php
@@ -5,11 +5,11 @@
  * Date: 25.12.2016
  * Time: 13:36
  */
-$mysqli = new mysqli('localhost', 'root','','FirstBD');
-if ($mysqli->connect_errno) {
-    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-$mysqli->set_charset("utf8");
+$mysqli = new mysqli('localhost', 'root','Rgrur4frg56eq16','derdata');
+//if ($mysqli->connect_errno) {
+//    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+//}
+//$mysqli->set_charset("utf8");
 if(file_exists('111.txt')) {
 
     $Ra;
@@ -20,6 +20,7 @@ if(file_exists('111.txt')) {
             if (strpos($line, "BEGIN") === false && strpos($line, "END") === false) {
                 if (strpos($line, "Ra") > 0) {
                     $raline = substr($line, strpos($line, "Ra"));
+
                 } else {
                     $raline = "";
                 }
@@ -28,19 +29,21 @@ if(file_exists('111.txt')) {
                     $exp = explode("&", $raline);
                     $exp[1] = (int)$exp[1];
                     $Ra = implode("", $exp);
+                    //echo $Ra.'<br>';
                 } else {
                     $line = str_replace(',', '.', $line);
                     $s = preg_replace("/[^0-9\.]/", "", $line);
                     $item = (float)$s;
-                    $sqls = "INSERT INTO ras(value, Ragroup) VALUES (" . $item . ",'" . $Ra . "');";
-                    $mysqli->query($sqls);
+                    //echo $item.'<br>';
+                    $sqls = "INSERT INTO ras(value, Ra) VALUES (" . $item . ",'" . $Ra . "');";
+//                    $mysqli->query($sqls);
                 }
 
             }
         }
-        $mysqli->commit();
+//        $mysqli->commit();
 echo "Готово)";
         fclose($handle);
-        unlink('111.txt');
+//        unlink('111.txt');
     }
 }
