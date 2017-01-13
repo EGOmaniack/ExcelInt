51313e2977259e6f58090dfb6ecd4ded58d7cb86
EGOmaniack <EGOmaniack@mail.ru>
2017-01-13 15:49:37 +1000

    v 0.003.0b Появился раздел Сетевые ресурсы Скрывается на всех браузерах кроме IE Пока что одна кнопка ведущая к изображениям

---
+++ web/bindex.php
@@ -0,0 +1,102 @@
+<?php
+ini_set('display_errors', 0) ;
+ini_set('xdebug.var_display_max_depth', 5);
+ini_set('xdebug.var_display_max_children', 256);
+ini_set('xdebug.var_display_max_data', 1024);
+
+require_once('BDgets.php');
+require_once('logsParser.php');
+
+$mergeLog = getlog('matmerge_versions_log.txt');
+$mlog = "<div id = 'verdiv'>";
+foreach($mergeLog as $key=> $value){
+    $mlog .='<p class = "vername">'.$value['ver'].'</p><ul>';
+    for($i = 0;$i < count($value['logs']);$i++){
+        $mlog .="<li>".$value['logs'][$i]."</li>";
+    }
+    $mlog .="</ul>";
+}
+$mlog .="</div>";
+$sek = strtotime("now");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Первая страничка</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/main.css?<?=$sek?>"/>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <link rel="icon" type="image/png" href="/pics/logo_min.png" />
+        <!--<script src="js/office.js"></script>-->
+        <!--<script src="https://appsforoffice.microsoft.com/lib/1/hosted/Office.js"/>-->
+
+    </head>
+    
+    <body>
+        <noscript>
+            Для полной функциональности этого сайта необходимо включить JavaScript.
+            Вот <a href="http://www.enable-javascript.com/ru/" target="_blank">
+            инструкции, как включить JavaScript в вашем браузере</ a>.
+        </noscript>
+        <div class="main">
+            <div id="top">
+            <p id = "print"></p>
+            </div>
+            <div class="container" id="mergemat">
+                <div class="blockhead">
+                    Объединить материалы <a id="mw"><?=$mergeLog[0]['ver']?></a>
+                </div>
+                <div class="thumb">
+                    <form name="excelCalc" method="post" action="find_mat_copy.php"
+                        enctype="multipart/form-data">
+                        <div class="text">Наш первый сервис позволяет объединять одинаковые
+                             материалы из ведомости материалов с учетом количества агрегатов. 
+                             <a href ="files/example.xlsx">Пример оформления заготовки</a><br>
+                        </div>
+                        <div class="text hiddeble">
+                             Файл принимается как минимум в формате Excel 2007 - "xlsx"
+                             Если планируется посчитать несколко агрегатов или др. инж. едениц, то:<br>
+                             Внутри, агрегаты должны быть указаны, как показано в <a href ="files/example_many.xlsx">этом</a> примере:<br>
+                             <lo><li> В колонке А ставится английское "n".</li>
+                             <li> В колонке B название агрегата</li>
+                             <li> В колонке С количество агрегатов</li></lo>
+                             Все в одну строку. Если у вас один агрегат его указывать необязательно.<br>
+                             Номер листа с материалами должен быть 4ым. Название листа не имеет значения.<br>
+                             Стили оформления не имеют значения
+                        </div>
+                        <input class="ShowHide" type="button" name="ShowHide" value="Развернуть/Свернуть">
+                        
+                        <div class="picplace">
+                            <input class = "picbtn" type="file" name="fileToUpload" >
+                        </div>
+                        <div >
+                            <p><input class = "submit btns" type="submit" value="Посчитать">
+                            <input class="btns reset" type="reset" value="Очистить"></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="container" id="pkbsrv">
+                <div class="blockhead">
+                    Сетевые ресурсы
+                </div>
+                <div class="thumb">
+                    <form action="file://dvgd-okbsrv-11/Print">
+                        <button class="btns submit" type="submit">Изображения</button>
+                    </form>
+                </div>
+            </div>
+            <div id="bottom">
+            
+            </div>
+        </div>
+        <p id = "ver"><?=getlog('all_ver_log.txt')[0]['ver']?> </p>
+        <div id="modal_form"><!-- Сaмo oкнo --> 
+            <span id="modal_close">X</span> <!-- Кнoпкa зaкрыть -->
+            <?=$mlog?> 
+            <!-- Тут любoе сoдержимoе -->
+        </div>
+        <div id="overlay"></div><!-- Пoдлoжкa -->
+    </body>
+    <script src="js/main.js?<?=$sek?>" type="text/javascript"></script>
+</html>
--- web/js/main.js
+++ web/js/main.js
@@ -1,37 +1,42 @@
+
+if(navigator.appName != "Microsoft Internet Explorer"){
+    $("#pkbsrv").hide();
+}
+
 $(document).ready( function() {
     var defaultcolor = 'black';
     printP('Добро пожаловать', '#58A963');
-
+    
     // сворачиваем разворачиваем текст
     $('.ShowHide').click(function(){
 		$('.hiddeble').toggleClass('hidden');
 	});
+    if(navigator.appName != "Microsoft Internet Explorer"){}
+    //сделать запись на экране
+    function printP (text, color){
+        $('#print').text(text).css('color', (color == undefined ? defaultcolor : color));
+    }
 
-//сделать запись на экране
-function printP (text, color){
-    $('#print').text(text).css('color', (color == undefined ? defaultcolor : color));
-}
 
-
-$('a#mw').click( function(event){ // лoвим клик пo ссылки с id="go"
-	event.preventDefault(); // выключaем стaндaртную рoль элементa
-	$('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
-		function(){ // пoсле выпoлнения предъидущей aнимaции
-			$('#modal_form') 
-				.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
-				.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
-	});
-});
-/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
-$('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
-	$('#modal_form')
-		.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
-			function(){ // пoсле aнимaции
-				$(this).css('display', 'none'); // делaем ему display: none;
-				$('#overlay').fadeOut(400); // скрывaем пoдлoжку
-			}
-		);
-});
+    $('a#mw').click( function(event){ // лoвим клик пo ссылки с id="go"
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+            function(){ // пoсле выпoлнения предъидущей aнимaции
+                $('#modal_form') 
+                    .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+                    .animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+        });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // делaем ему display: none;
+                    $('#overlay').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
+    });
 
 
 
