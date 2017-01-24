53711677258eb1efb0a8ab7cb308fcf0638b53b5
EGOmaniack <EGOmaniack@mail.ru>
2017-01-25 09:21:30 +1000

    it's working !!

--- web/pgBD.php
+++ web/pgBD.php
@@ -1,28 +1,25 @@
 <?php
-echo 111;
+//phpinfo();
     function connect (){
-        $dbconn = pg_connect(
-            "host=localhost port=5432
-            dbname=thedata user=postgres
-            password=Rgrur4frg56eq16")
+        echo "Trying connect"."<br>";
+        $dbconn = pg_connect( "host=localhost port=5432 dbname=thedata user=postgres password=Rgrur4frg56eq16")
             or die('Could not connect: ' . pg_last_error());
-echo 222;
+        echo "Connected"."<br>";
+        $result = pg_query("select * from  dictionary.project_object_type") or die('Ошибка запроса: ' . pg_last_error()); 
 
-        // $result = pg_query("select * from  directory.time_period_measure") or die('Ошибка запроса: ' . pg_last_error()); 
-
-        // echo "<table>\n";
-        // while ($line = pg_fetch_assoc($result)) {
-        //     echo "\t<tr>\n";
-        //     foreach ($line as $col_key => $col_value ) {
-        //         echo "\t\t<td>$col_key: $col_value;</td>\n";
-        //     }
-        //     echo "\t</tr>\n";
-        // }
-        // echo "</table>\n";
-        // pg_free_result($result);
+        echo "<table>\n";
+        while ($line = pg_fetch_assoc($result)) {
+            echo "\t<tr>\n";
+            foreach ($line as $col_key => $col_value ) {
+                echo "\t\t<td>$col_key: $col_value;</td>\n";
+            }
+            echo "\t</tr>\n";
+        }
+        echo "</table>\n";
+        pg_free_result($result);
         pg_close($dbconn);
     }
-
+// connect();
 try {
     connect();
 } catch (Exception $e) {
